0223dfda84d58ae8665440e869b2899f3d16950f
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2013-10-01 23:11:37 +0530

    Move custom-file load code a little up.

--- init.el
+++ init.el
@@ -14,6 +14,11 @@
 ;; http://whattheemacsd.com/init.el-01.html#comment-742017081
 (mouse-wheel-mode -1)
 
+;; Emacs saves settings set from the "customize" UI here.
+;; https://github.com/purcell/emacs.d/blob/master/init.el
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 (add-to-list 'load-path user-emacs-directory)
 
@@ -28,9 +33,3 @@
 ;; Default font
 ;; http://stackoverflow.com/a/6050987
 (set-default-font "Ubuntu Mono 15")
-
-;; Emacs saves settings set from the "customize" UI here.
-;; https://github.com/purcell/emacs.d/blob/master/init.el
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
