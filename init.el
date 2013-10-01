b8c33e0f36fba2427d9f4426798883c5c8f4e769
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2013-10-01 20:53:57 +0530

    Disable mouse-wheel-mode.

--- init.el
+++ init.el
@@ -8,3 +8,7 @@
 
 ;; No splash screen please ... jeez
 (setq inhibit-startup-message t)
+
+;; http://whattheemacsd.com/init.el-01.html#comment-742017081
+;; Disable mouse-wheel-mode; very annoying.
+(mouse-wheel-mode -1)
