65b74f663afc97e137962fd1ff7bacfb050873ee
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2013-10-01 21:20:20 +0530

    Move reference links below the explanation text.

--- init.el
+++ init.el
@@ -1,16 +1,17 @@
 ;; Essentials
 
-;; http://whattheemacsd.com/init.el-01.html
 ;; Turn off mouse interface early in startup to avoid momentary display
+;; http://whattheemacsd.com/init.el-01.html
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 ;; No splash screen please ... jeez
+;; http://whattheemacsd.com/init.el-01.html
 (setq inhibit-startup-message t)
 
-;; http://whattheemacsd.com/init.el-01.html#comment-742017081
 ;; Disable mouse-wheel-mode; very annoying.
+;; http://whattheemacsd.com/init.el-01.html#comment-742017081
 (mouse-wheel-mode -1)
 
 ;; Default font
